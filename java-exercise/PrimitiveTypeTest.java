2ff9c5844ccb7ad2bace88e59fbd4a785de7dfcb
lujinjianst <lu605894472@gmail.com>
2012-12-30 15:37:10 +0800

    add two programs

---
+++ java-exercise/PrimitiveTypeTest.java
@@ -0,0 +1,56 @@
+
+public class PrimitiveTypeTest {
+	public static void main(String[] args)
+	{
+//		byte
+		System.out.println("base type:byte  binary equivalent:" + Byte.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + Byte.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + Byte.MIN_VALUE);
+		System.out.println();
+		
+//		short
+		System.out.println("base type:short  binary equivalent:" + Short.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + Short.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + Short.MIN_VALUE);
+		System.out.println();
+		
+//		int
+		System.out.println("base type:integer  binary equivalent:" + Integer.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + Integer.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + Integer.MIN_VALUE);
+		System.out.println();
+		
+//		long
+		System.out.println("base type:long  binary equivalent:" + Long.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + Long.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + Long.MIN_VALUE);
+		System.out.println();
+		
+//		float
+		System.out.println("base type:float  binary equivalent:" + Float.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + Float.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + Float.MIN_VALUE);
+		System.out.println();
+		
+//		double
+		System.out.println("base type:double  binary equivalent:" + Double.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + Double.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + Double.MIN_VALUE);
+		System.out.println();
+		
+//		char
+		System.out.println("base type:char  binary equivalent:" + Character.SIZE);
+		System.out.println("Wrap class:java.lang.Byte");
+		System.out.println("Max value:Byte.MAX_VALUE=" + (int)Character.MAX_VALUE);
+		System.out.println("Min value:Byte.MIN_VALUE=" + (int)Character.MIN_VALUE);
+		System.out.println();
+		
+	}
+}
+
