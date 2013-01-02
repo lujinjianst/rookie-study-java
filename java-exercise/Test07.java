499e5bddaf7672a1669dd48d221a4509590e573a
lujinjianst <lu605894472@gmail.com>
2013-01-02 22:10:17 +0800

    Displacement operator practice

---
+++ java-exercise/Test07.java
@@ -0,0 +1,13 @@
+public class Test07
+{
+	public static void main(String args[])
+	{
+		int n = 7 >> 2;
+		int m = 7 << 2;
+		int i = 7 >>> 2;
+
+		System.out.println(n);
+		System.out.println(m);
+		System.out.println(i);
+	}
+}
