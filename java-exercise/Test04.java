651a7184bc90b2dcb103955a25a566b0c52cad3e
lujinjianst <lu605894472@gmail.com>
2013-01-02 22:07:54 +0800

    Logical operator practice

---
+++ java-exercise/Test04.java
@@ -0,0 +1,12 @@
+public class Test04
+{
+	public static void main(String args[])
+	{
+		boolean n = (4 > 3)&&(2<8);
+		boolean m = (4 > 3)||(2<8);
+		boolean h = !(2<8);
+		System.out.println(n);
+		System.out.println(m);
+		System.out.println(h);
+	}
+}
