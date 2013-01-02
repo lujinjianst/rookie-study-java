cd9a268de209f65f2e4903949df35bce720266e2
lujinjianst <lu605894472@gmail.com>
2013-01-02 22:11:14 +0800

    Displacement operator practice

---
+++ java-exercise/Test08.java
@@ -0,0 +1,14 @@
+public class Test08
+{
+	public static void main (String args[])
+	{
+		int n = 7;
+		int j = 1;
+		j += n;
+		System.out.println(j);
+		j -= n;
+		System.out.println(j);
+		j *= n;
+		System.out.println(j);
+	}
+}
