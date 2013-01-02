03f7f67b7e5ec8dd07b7c2e9ca93a73326e04c56
lujinjianst <lu605894472@gmail.com>
2013-01-02 22:09:41 +0800

    An operator practice

---
+++ java-exercise/Test06.java
@@ -0,0 +1,13 @@
+public class Test06
+{
+	public static void main(String args[])
+	{
+		// 0000 0100 & 0000 0011
+		int n = 4 & 3;
+		int m = 4 | 3;
+		int i = 4 ^ 3;
+		System.out.println(n);
+		System.out.println(m);
+		System.out.println(i);
+	}
+}
